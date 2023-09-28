4ac2f7c33d30a3d69038a7b9abb332037bf44797
Johann Schopplich <mail@johannschopplich.com>
2023-09-28 18:45:56 +0200

    chore: add medium zoom wrapper

---
+++ src/components/MediumZoom.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { watch } from "vue";
+import mediumZoom from "medium-zoom";
+import type { ComponentPublicInstance } from "vue";
+import type { Zoom, ZoomOptions } from "medium-zoom";
+
+const props = defineProps<{
+  options?: ZoomOptions;
+}>();
+
+let zoom: Zoom | undefined = undefined;
+
+watch(
+  () => props.options,
+  (options) => {
+    const zoom = getZoom();
+    zoom.update(options || {});
+  },
+);
+
+function getZoom() {
+  return (zoom ??= mediumZoom(props.options));
+}
+
+function attachZoom(ref: Element | ComponentPublicInstance | null) {
+  const image = ref as HTMLImageElement | null;
+  const zoom = getZoom();
+
+  if (image) {
+    zoom.attach(image);
+  } else {
+    zoom.detach();
+  }
+}
+</script>
+
+<template>
+  <img :ref="attachZoom" />
+</template>
