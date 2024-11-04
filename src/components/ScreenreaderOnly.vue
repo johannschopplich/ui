8b4fb7d502c5c9c333767217a562da7d8f6d551d
Johann Schopplich <mail@johannschopplich.com>
2024-11-04 15:42:13 +0100

    feat: expose `ScreenreaderOnly`

---
+++ src/components/ScreenreaderOnly.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import type { PrimitiveProps } from "./Primitive";
+import { Primitive } from "./Primitive";
+
+export interface VisuallyHiddenProps extends PrimitiveProps {}
+</script>
+
+<script setup lang="ts">
+withDefaults(defineProps<VisuallyHiddenProps>(), { as: "span" });
+</script>
+
+<template>
+  <Primitive :as="as" :as-child="asChild" class="sr-only">
+    <slot />
+  </Primitive>
+</template>
