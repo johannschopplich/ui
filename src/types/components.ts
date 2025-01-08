49c50752085a08ec64069d1d9c78156a7c64c181
Johann Schopplich <mail@johannschopplich.com>
2025-01-08 07:41:52 +0100

    chore: fix build

--- src/types/components.ts
+++
@@ -1,6 +0,0 @@
-import type { ComponentPublicInstance } from "vue";
-
-export type ComponentConstructor<
-  T extends ComponentPublicInstance<Props> = ComponentPublicInstance<any>,
-  Props = any,
-> = new (...args: any[]) => T;
