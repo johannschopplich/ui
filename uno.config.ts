97d45a83a7a273610a6c195a1e22e92f1ee9bd5e
Johann Schopplich <mail@johannschopplich.com>
2025-01-28 11:11:01 +0100

    chore: upgrade dependencies

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,5 @@
-import { presetForms } from "@julr/unocss-preset-forms";
 import { defineConfig, presetIcons, presetWind } from "unocss";
 
 export default defineConfig({
-  presets: [presetWind(), presetForms(), presetIcons()],
+  presets: [presetWind(), presetIcons()],
 });
